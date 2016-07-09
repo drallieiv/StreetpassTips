ceba0e5445f19e1fb4acb1014b09001a803cfd21
Drallieiv <knabyss@gmail.com>
2016-07-09 21:39:20 +0200

    Implement bait display

--- app/components/fish-bait-detail.js
+++ app/components/fish-bait-detail.js
@@ -2,20 +2,18 @@ import Ember from 'ember';
 import {dbFishing}  from './../data/fishing';
 
 export default Ember.Component.extend({
+
   init() {
     this._super(...arguments);
 
     var allBaits = dbFishing.getAllBaits();
     var baitDetail = [];
-    allBaits.forEach(function (element) {
-
-      //TODO Search Letter in bais
-      var active = (Math.random() > 0.5) ? true : false;
-
-      baitDetail.push({color: element.hexcolor, active:active});
+    allBaits.forEach( (bait) =>{
+      let active = this.get('fish').hasBait(bait);
+      baitDetail.push({color: bait.hexcolor, id:bait.id, active:active});
     });
-    this.set('baitDetail', baitDetail);
 
+    this.set('baitDetail', baitDetail);
   },
 
 });
--- app/models/fishing/fish.js
+++ app/models/fishing/fish.js
@@ -7,5 +7,15 @@ export default Ember.Object.extend({
   type: '',       // First letter of type (CLRD)  for: Coast, Lake/Pond, River, Deep-Sea
   baits: [],      // Baits that the fish likes
   points: 0,      // Number of xp points for catching that fish
-  gold: 0         // Money won by catching that fish
+  gold: 0,         // Money won by catching that fish
+
+  hasBait: function(searchedBait){
+    if(Ember.typeOf(searchedBait) === 'instance'){
+      return this.get('baits').find( b => b === searchedBait) !== undefined;
+    }else if(typeof searchedBait === 'string'){
+      return this.get('baits').find( b => b.id === searchedBait) !== undefined;
+    }
+    return false;
+  },
+
 });
